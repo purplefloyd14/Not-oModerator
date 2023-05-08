f06b60c0eb3e0e629a5a2161cba2b2600359d53f
Bradley Neal <bradleyhneal@gmail.com>
2023-05-08 11:24:05 -0400

    makes it work with mods called bot

--- contentScript.js
+++ contentScript.js
@@ -1,30 +1,14 @@
 // contentScript.js
 
 function removeAutoModeratorDivs() {
-    const autoModeratorLinks = document.querySelectorAll('a.author');
-  
-    autoModeratorLinks.forEach((link) => {
-      if (link.textContent === 'AutoModerator') {
-        const grandparentDiv = link.parentElement.parentElement;
-        grandparentDiv.remove();
-      }
-    });
-  }
-  
-  function observeDOMChanges() {
-    const observer = new MutationObserver((mutationsList) => {
-      for (const mutation of mutationsList) {
-        if (mutation.type === 'childList') {
-          removeAutoModeratorDivs();
-        }
-      }
-    });
-  
-    observer.observe(document.body, { childList: true, subtree: true });
-  }
-  
-  // Wait for the DOMContentLoaded event before setting up the MutationObserver
-  document.addEventListener('DOMContentLoaded', () => {
-    observeDOMChanges();
+  const autoModeratorLinks = document.querySelectorAll('a.author.moderator');
+
+  autoModeratorLinks.forEach((link) => {
+    if (link.textContent.toLowerCase().includes('mod') || link.textContent.toLowerCase().includes('bot')) {
+      const grandparentDiv = link.parentElement.parentElement.parentElement;
+      grandparentDiv.remove();
+    }
   });
-  
+}
+
+removeAutoModeratorDivs();
