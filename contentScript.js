f9ad934d92ca2090354fb3f759fbd1f9548073fb
Bradley Neal <bradleyhneal@gmail.com>
2023-05-08 14:23:59 -0400

    adds for case of vote

--- contentScript.js
+++ contentScript.js
@@ -4,7 +4,7 @@ function removeAutoModeratorDivs() {
   const autoModeratorLinks = document.querySelectorAll('a.author.moderator');
 
   autoModeratorLinks.forEach((link) => {
-    if (link.textContent.toLowerCase().includes('mod') || link.textContent.toLowerCase().includes('bot')) {
+    if (link.textContent.toLowerCase().includes('mod') || link.textContent.toLowerCase().includes('bot') || link.textContent.toLowerCase().includes('vote')) {
       const grandparentDiv = link.parentElement.parentElement.parentElement;
       grandparentDiv.remove();
     }
